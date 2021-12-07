86e7b4d674ef29154b0aabb86ab2fd3c500836a7
laneia <laneiamurray@gmail.com>
2021-12-07 09:20:35 -0800

    restructured the logic in app.js to fix overlay blocking background elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './styles/App.css';
 import Landing from './pages/Landing'
 import About from './pages/About'
@@ -9,28 +10,44 @@ import Contact from './pages/Contact'
 import SideNav from './components/SideNav'
 import TopNav from './components/TopNav';
 
-function App() {
-  return (
-    <div className="App">
+
+const overlay = (
+  <div>
       <div className="landing-overlay">
         <div className="owl-overlay"></div>
       </div>
-      
+  </div>
+)
 
-      <TopNav />
+const current = (
+  <div className="App">
+    <TopNav />
 
-      <div className="bottom-app">
-        <SideNav />
-        <main id="main">
-          <Landing/>
-          <About/>
-          <Experience/>
-          <Skills/>
-          <Projects/>
-          <Contact/>
-        </main>
-      </div>  
-    </div>
+    <div className="bottom-app">
+      <SideNav />
+      <main id="main">
+        <Landing/>
+        <About/>
+        <Experience/>
+        <Skills/>
+        <Projects/>
+        <Contact/>
+      </main>
+    </div>  
+  </div>
+)
+
+
+function App() {
+  const [rendered, setRendered] = useState(overlay)
+  
+  useEffect(() => {
+    const timer = setTimeout(() => setRendered(current), 3700);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    rendered
   );
 }
 
