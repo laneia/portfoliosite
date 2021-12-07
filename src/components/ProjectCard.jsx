a445cc2bc2844c5db8aa084c6bdad39869356ac6
laneia <laneiamurray@gmail.com>
2021-12-07 11:57:55 -0800

    contact page styled, started working on projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,16 @@ const ProjectCard = ({title, description, photo, link, technologies}) => {
 
     return (
         <div className="project-card">
-            <a href={link} target="_blank" rel="noreferrer">
-                <div className="project-container">
+            {/* <a href={link} target="_blank" rel="noreferrer"> */}
+                <div className="project-img-cont">
                     <img src={photo} alt="projects you cant see" />
-                    <div className="project-info">
+                    <div className="project-details">
                         <h1>{title}</h1>
                         <h2>{technologies}</h2>
                         <p>{description}</p>
-                    </div>
+                    </div>            
                 </div>
-            </a>
+            {/* </a> */}
         </div>
     )
 }
