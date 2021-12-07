04e632e795db543c4cd07aa69664ce1d4bd1cdba
laneia <laneiamurray@gmail.com>
2021-12-06 19:53:01 -0800

    landing page styled

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,6 @@ const Landing = () => {
 
     return (
         <div id="landing">
-            {/* <img src="https://i.imgur.com/lMuekL1.jpg" alt="landing-owl"/> */}
             <div className="branding">
                 <h1>this is a brand statement</h1>
                 <p>stuff about me goes here look how interesting bitch</p>
